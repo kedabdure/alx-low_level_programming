0b252231aa2dfe374bb344a47c9d978a403d54bb
kedabdure <abdurehimk77@gmail.com>
2023-06-15 14:59:52 -0700

    4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,2 +1,2 @@
 #!/bin/bash
-echo 'int main(void) { puts("Programming is like building a multilingual puzzle"); return 0; }' > program.c
+echo '#include <stdio.h> int main(void) { puts("Programming is like building a multilingual puzzle"); return 0; }' > program.c

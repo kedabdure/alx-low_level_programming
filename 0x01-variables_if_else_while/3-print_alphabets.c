8afc74bcf768fdfded393fb8da8363a5d5985785
kedabdure <abdurehimk77@gmail.com>
2023-06-16 11:31:28 -0700

    3

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -13,12 +13,12 @@ int main(void)
 
         for (letter = 'a'; letter <= 'z'; letter++)
         {
-                putchar(letter);                                   }
+                putchar(letter);
+	}
         for (letter = 'A'; letter <= 'Z'; letter++)
         {
-                putchar(letter);
-        }
-							      	   putchar('\n'); 
+		putchar('\n');
+	}
 
         return (0);
 }

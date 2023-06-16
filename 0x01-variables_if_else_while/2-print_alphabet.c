4b69cc5b037dd61344689febef71a55718825802
kedabdure <abdurehimk77@gmail.com>
2023-06-16 10:02:24 -0700

    2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - Entry point
+ *
+ * Return: 0 (Success)
+*/
+int main(void)
+{
+	char letter;
+	for (letter = 'a'; letter <= 'z'; letter++) 
+	{
+		putchar(letter);
+	}
+		putchar('\n');
+
+	return (0);
+}

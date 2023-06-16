e24cd4c451377d7c96bff526ae29035f073ae447
kedabdure <abdurehimk77@gmail.com>
2023-06-16 11:23:27 -0700

    3

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -10,21 +10,6 @@ int main(void)
 {
         char letter;
 
-                                                                   for (letter = 'a'; letter <= 'z'; letter++)
-        {
-                putchar(letter);                                   }
-#include <stdlib.h>
-#include <time.h>
-#include <stdio.h>
-/**
- * main - Entry point
- *
- * Return: 0 (Success)
-*/
-int main(void)
-{
-        char letter;
-
 
         for (letter = 'a'; letter <= 'z'; letter++)
         {

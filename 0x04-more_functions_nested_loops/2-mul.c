63c6c843ca286c1df160dc6e35dccd8a9099d815
kedabdure <abdurehimk77@gmail.com>
2023-06-22 06:36:51 -0700

    updated

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -1,18 +1,16 @@
 #include "main.h"
-/**
- *print_alphabet_x10 - function to print 10 times the alphabet
- * Return: Always 0
- */
-void print_alphabet_x10(void)
-{
-	char c, i;
 
-	for (i = 0; i <= 9; i++)
-	{
-		for (c = 'a'; c <= 'z'; c++)
-		{
-			_putchar(c);
-		}
-		_putchar('\n');
-	}
+/**
+ * mul - multiplies two integers
+ * @a: int to be multiplied to b
+ * @b: int to be multiplied to a
+ * Return: the result of the operation
+ */
+int mul(int a, int b)
+{
+	int c;
+
+	c = a * b;
+
+	return (c);
 }

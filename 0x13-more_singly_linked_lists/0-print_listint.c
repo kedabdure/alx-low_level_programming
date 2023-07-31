c199ae3abb42edbb572610e44feb6716d34ff3a6
kedabdure <abdurehimk77@gmail.com>
2023-07-31 13:13:53 -0700

    committed

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -1,18 +1,17 @@
 #include "lists.h"
 /**
  * print_listint - print the number of nodes
- * @h: accepts the first node address
+ * @h: holds the first node address
  *
  * Return: the number of nodes
  */
 size_t print_listint(const listint_t *h)
 {
-	const listint_t *temp = h;
-	int count = 0;
+	size_t count = 0;
 
-	while(temp != NULL)
+	while(h != NULL)
 	{
-		temp = temp->next;
+		h = h->next;
 		count++;
 	}
 

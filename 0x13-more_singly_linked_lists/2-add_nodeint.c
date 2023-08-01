cf025f706812f92707cc28e71f1d83248543c56e
kedabdure <abdurehimk77@gmail.com>
2023-08-01 00:19:42 -0700

    committed

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -6,12 +6,16 @@
  * @head: hold  address of head pointer
  * @n: receive intiger value
  *
- * Return: return the address of the new node
+ * Return: the last added element
  */
 
 listint_t *add_nodeint(listint_t **head, const int n)
 {
-	listint_t *temp = malloc(sizeof(listint_t));
+	listint_t *temp;
+
+	if (head == NULL)
+		return (NULL);
+	temp = malloc(sizeof(listint_t));
 
 	if (temp == NULL)
 	{

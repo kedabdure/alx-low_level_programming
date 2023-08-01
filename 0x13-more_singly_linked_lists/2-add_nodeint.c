03fc53648ad30778ce4a0da8f0fb60cd25b906bb
kedabdure <abdurehimk77@gmail.com>
2023-08-01 00:06:16 -0700

    committed

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -11,13 +11,7 @@
 
 listint_t *add_nodeint(listint_t **head, const int n)
 {
-	listint_t *temp;
-
-	if (*head == NULL)
-	{
-		return (NULL);
-	}
-	temp = malloc(sizeof(listint_t));
+	listint_t *temp = malloc(sizeof(listint_t));
 
 	if (temp == NULL)
 	{

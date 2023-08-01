e41ed3b69394b42b5a27d5f7c89447d63bc9ec96
kedabdure <abdurehimk77@gmail.com>
2023-08-01 11:24:16 -0700

    committed

---
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -0,0 +1,26 @@
+#include "lists.h"
+
+/**
+ * get_nodeint_at_index - Rerurn the node at a given index.
+ * @head: poinetr that holds head address.
+ * @index: used to select node
+ *
+ * Return: indexed node
+ *
+ */
+
+listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
+{
+	int i;
+
+	if (head == NULL)
+		return (NULL);
+	for (i = 0; i < index; i++)
+	{
+		head = head->next;
+		if (head == NULL)
+			return (NULL);
+	}
+
+	return (head);
+}

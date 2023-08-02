1e0454b0e43f0f7f65e474f459d1358a93716951
kedabdure <abdurehimk77@gmail.com>
2023-08-01 18:22:02 -0700

    committed

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -32,7 +32,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	else
 	{
 		ptr = *head;
-		for (i = 0; i < idx - 1; i++)
+		for (i = 0; i < idx - 1 && ptr != NULL; i++)
 		{
 			ptr = ptr->next;
 		}

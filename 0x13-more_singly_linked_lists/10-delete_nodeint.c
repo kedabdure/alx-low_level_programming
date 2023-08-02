5a60c14bc4819b2c5307b4d8608e23e99e8634f8
kedabdure <abdurehimk77@gmail.com>
2023-08-01 19:29:34 -0700

    committed

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -32,7 +32,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 				return (-1);
 		}
 		temp = ptr->next;
-		ptr-next = temp-next;
+		ptr->next = temp->next;
 		free(temp);
 	}
 	return (1);

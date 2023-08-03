9d424dbea9e63215dbd33efb87458ef2f07e244d
kedabdure <abdurehimk77@gmail.com>
2023-08-03 15:59:59 -0700

    committed

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * binary_to_uint - convert binary to unsigned int
+ * @b: pointer point8ng to string
+ *
+ * Return: converted number
+ */
+
+unsigned int binary_to_uint(const char *b)
+{
+	int i;
+	unsigned int k;
+
+	k = 0;
+	if (b == NULL)
+		return (0);
+	for (i = 0; b[i] != '\0'; i++)
+	{
+		if (b[i] != '0' && b[i] != '1')
+			return (0);
+	}
+	for (i = 0; b[i] != '\0'; i++)
+	{
+		k = k << 1;
+		if (b[i] == '1')
+			k = k + 1;
+	}
+	return(k);
+
+}

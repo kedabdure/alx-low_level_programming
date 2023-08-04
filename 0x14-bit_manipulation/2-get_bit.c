20496c2be477b6c2d0e3fe53e5d2963cee4331e5
kedabdure <abdurehimk77@gmail.com>
2023-08-03 17:33:47 -0700

    committed

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -12,7 +12,7 @@ int get_bit(unsigned long int n, unsigned int index)
 {
 	unsigned long int mask;
 
-	if (index > (sizeof(unsigned long int) * 8 - 1))
+	if (index > (sizeof(unsigned long int) * 8 - 1)
 			return (-1);
 
 	mask = 1UL << index;

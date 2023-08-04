6aee7f88aaf3a9893c5fd42e9aacadc12403a0e4
kedabdure <abdurehimk77@gmail.com>
2023-08-04 06:45:36 -0700

    committed

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * flip_bits - return the number of bit you would need to flip from one number to another
+ * flip_bits - return the number of bit you would need to flip
  * @n: the first number
  * @m: the second number
  *
@@ -11,18 +11,17 @@
 unsigned int flip_bits(unsigned long int n, unsigned long int m)
 {
 	unsigned long int difference, result;
-	unsigned int cout_flips = 0, i;
+	unsigned int count_flip = 0, i;
 
 	difference = n ^ m;
 	result = 1;
 
 	for (i = 0; i < (sizeof(unsigned long int) * 8); i++)
 	{
-		if(result == (differnce & result)
-			j++;
+		if (result == (difference & result))
+			count_flip++;
 		result <<= 1;
 	}
 
-	return (j);
+	return (count_flip);
 }
-

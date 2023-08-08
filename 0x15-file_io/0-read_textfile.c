0ccdcae31dcffb78b4d5f88634b8619ca3238d30
kedabdure <abdurehimk77@gmail.com>
2023-08-07 19:22:50 -0700

    committed

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,5 +1,7 @@
-#include "main.h"
+#include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
+#include <fcntl.h>
 
 /**
  * custom_read_textfile - Reads a text file and displays it on standard output.

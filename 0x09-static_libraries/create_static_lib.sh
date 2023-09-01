4478d3a0a732a27cc44534b0499bd7a17275b232
kedabdure <abdurehimk77@gmail.com>
2023-09-01 16:58:01 -0700

    committed

--- 0x09-static_libraries/create_static_lib.sh
+++ 0x09-static_libraries/create_static_lib.sh
@@ -1,11 +1,5 @@
 #!/bin/bash
-
-# Compile all .c files in the current directory into object files
 gcc -c *.c
-
-# Create a static library named liball.a from the generated object files
 ar rcs liball.a *.o
-
-# Clean up by removing the object files
 rm *.o
 

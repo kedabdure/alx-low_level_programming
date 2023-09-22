c9978dcfe86dedead8c578e2d5d13731c9ee1b8a
kedabdure <abdurehimk77@gmail.com>
2023-09-21 19:20:38 -0700

    committed

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -7,7 +7,7 @@ void print_before_main(void) __attribute__((constructor));
  */
 void print_before_main(void)
 {
-    printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+	printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
 }
 
 /**
@@ -16,7 +16,7 @@ void print_before_main(void)
  */
 int main(void)
 {
-    printf("(A tortoise, having pretty good sense of a hare's nature, challenges one to a race.)\n");
-    return (0);
+	printf("(A tortoise, having pretty good sense of a hare's nature, challenges one to a race.)\n");
+	return (0);
 }
 

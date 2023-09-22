a23b6673b4771e09ef6c5534efdce4796e4fb8f6
kedabdure <abdurehimk77@gmail.com>
2023-09-21 19:27:32 -0700

    committed

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,22 +1,16 @@
 #include <stdio.h>
 
-void print_before_main(void) __attribute__((constructor));
+/**
+ * printBeforeMain - Function to print a message before main.
+ */
+void printBeforeMain(void) __attribute__((constructor));
 
 /**
- * print_before_main - Function to print a message before main.
+ * printBeforeMain - Implementation of printBeforeMain.
  */
-void print_before_main(void)
+void printBeforeMain(void)
 {
-	printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
-}
-
-/**
- * main - Entry point of the program.
- * Return: Always 0.
- */
-int main(void)
-{
-	printf("(A tortoise, having pretty good sense of a hare's nature, challenges one to a race.)\n");
-	return (0);
+    printf("You're beat! and yet, you must allow,\n");
+    printf("I bore my house upon my back!\n");
 }
 

5c6df01c05a93fb8596e7f1643b672ce63a5b3ad
kedabdure <abdurehimk77@gmail.com>
2023-10-05 05:05:40 -0700

    committed

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -1,37 +1,40 @@
 #include "main.h"
 
 /**
- * create_file - creates a file and writes text content to it
- * @filename: name of the file to create
- * @text_content: NULL-terminated string to write to the file
- *
- * Return: 1 on success, -1 on failure
+ * _str_len - Calculate string length.
+ * @str: Pointer to the string.
+ * Return: Length of the string.
  */
-int create_file(const char *filename, char *text_content)
+size_t _str_len(char *str)
 {
-	int file_descriptor, bytes_written, length = 0;
+	size_t i;
+
+	for (i = 0; str[i]; i++)
+		;
+	return (i);
+}
+
+/**
+ * append_text - Appends text to the end of a file.
+ * @filename: Name of the file.
+ * @content: NULL terminated string to add at the end of the file.
+ * Return: 1 on success, -1 on failure.
+ */
+int append_text(const char *filename, char *content)
+{
+	int fd;
+	ssize_t written_bytes;
 
 	if (filename == NULL)
 		return (-1);
-
-	if (text_content != NULL)
-	{
-		while (text_content[length])
-			length++;
-	}
-
-	file_descriptor = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
-	if (file_descriptor == -1)
+	fd = open(filename, O_WRONLY | O_APPEND);
+	if (fd == -1)
 		return (-1);
-
-	bytes_written = write(file_descriptor, text_content, length);
-	if (bytes_written == -1)
-	{
-		close(file_descriptor);
+	if (content != NULL)
+		written_bytes = write(fd, content, _str_len(content));
+	close(fd);
+	if (written_bytes == -1)
 		return (-1);
-	}
-
-	close(file_descriptor);
 	return (1);
 }
 

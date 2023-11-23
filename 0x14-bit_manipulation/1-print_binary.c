59492c51b4b9eebb3876db379bcb547087b95b63
kedabdure <abdurehimk77@gmail.com>
2023-11-23 11:47:16 -0800

    committed

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -2,26 +2,25 @@
 #include "main.h"
 
 /**
- *
- *
- *
+ * print_binary - Prints the binary representation of a number
+ * @n: The number to be converted to binary and printed
  */
-
 void print_binary(unsigned long int n)
 {
-	int bin = 0, rem = 0, place = 1;
+    int bin = 0, rem = 0, place = 1;
 
-	while (n)
-	{
-		rem = n%2;
-		n = n/2;
-		bin = bin + (rem * place);
-		place = place * 10;
-	}
+    while (n)
+    {
+        rem = n % 2;
+        n = n / 2;
+        bin = bin + (rem * place);
+        place = place * 10;
+    }
 
-	while (bin > 0) {
+    while (bin > 0)
+    {
         _putchar((bin % 10) + '0');
         bin /= 10;
     }
-
 }
+

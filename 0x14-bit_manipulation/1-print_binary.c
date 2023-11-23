b14882fab160d45a6c4a95458fdfbc38ff8046cf
kedabdure <abdurehimk77@gmail.com>
2023-11-23 12:02:26 -0800

    committed

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -1,26 +1,48 @@
-#include <stdio.h>
 #include "main.h"
 
 /**
- * print_binary - Prints the binary representation of a number
- * @n: The number to be converted to binary and printed
+ * power - Calculate the power of a number
+ * @base: The base number
+ * @exponent: The exponent
+ * Return: The result of base raised to the power of the exponent
  */
-void print_binary(unsigned long int n)
+unsigned long int power(unsigned int base, unsigned int exponent)
 {
-    int bin = 0, rem = 0, place = 1;
+	unsigned long int result = 1;
+	unsigned int i;
 
-    while (n)
-    {
-        rem = n % 2;
-        n = n / 2;
-        bin = bin + (rem * place);
-        place = place * 10;
-    }
+	for (i = 0; i < exponent; i++)
+	{
+		result *= base;
+	}
 
-    while (bin > 0)
-    {
-        _putchar((bin % 10) + '0');
-        bin /= 10;
-    }
+	return result;
+}
+
+/**
+ * print_binary - Print the binary representation of a number
+ * @num: The number to be converted to binary and printed
+ */
+void print_binary(unsigned long int num)
+{
+	unsigned long int divisor, temp;
+	char flag = 0;
+
+	divisor = power(2, sizeof(unsigned long int) * 8 - 1);
+
+	while (divisor > 0)
+	{
+		temp = num & divisor;
+		if (temp == divisor)
+		{
+			flag = 1;
+			_putchar('1');
+		}
+		else if (flag == 1 || divisor == 1)
+		{
+			_putchar('0');
+		}
+		divisor >>= 1;
+	}
 }
 

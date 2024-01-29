1675c8b1716ee02536f39d8b3504bab43549e4ff
kedabdure <abdurehimk77@gmail.com>
2024-01-29 14:26:50 -0800

    new file created

---
+++ 0x18-dynamic_libraries/100-operations.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+int add(int a, int b) {
+    return a + b;
+}
+
+int sub(int a, int b) {
+    return a - b;
+}
+
+int mul(int a, int b) {
+    return a * b;
+}
+
+int div(int a, int b) {
+    if (b != 0) {
+        return a / b;
+    } else {
+        return 0; // Handling division by zero
+    }
+}
+
+int mod(int a, int b) {
+    if (b != 0) {
+        return a % b;
+    } else {
+        return 0; // Handling modulo by zero
+    }
+}
+

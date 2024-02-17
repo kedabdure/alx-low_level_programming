488133bb2da34aa5ac8e931cafd25c6385aa8df8
Abdurehim <128035951+kedabdure@users.noreply.github.com>
2024-02-17 04:03:32 +0300

    Update hash_tables.h

--- 0x1A-hash_tables/hash_tables.h
+++ 0x1A-hash_tables/hash_tables.h
@@ -24,8 +24,7 @@ typedef struct hash_node_s
  * struct hash_table_s - Hash table data structure
  *
  * @size: The size of the array
- * @array: An array of size @size
- *         Each cell of this array is a pointer to the first node of a linked list.
+ * @array: array of pointer to store the key-value pair
  */
 typedef struct hash_table_s
 {

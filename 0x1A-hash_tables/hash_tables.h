311dc507bb95dbf8fb06bc0cf8e5ba023da7d57d
Abdurehim <128035951+kedabdure@users.noreply.github.com>
2024-02-17 03:59:19 +0300

    Update hash_tables.h

--- 0x1A-hash_tables/hash_tables.h
+++ 0x1A-hash_tables/hash_tables.h
@@ -9,15 +9,15 @@
  * struct hash_node_s - Node of a hash table
  *
  * @key: The key, string
- * The key is unique in the HashTable
+ *        The key is unique in the HashTable
  * @value: The value corresponding to a key
  * @next: A pointer to the next node of the List
  */
 typedef struct hash_node_s
 {
-     char *key;
-     char *value;
-     struct hash_node_s *next;
+	char *key;
+	char *value;
+	struct hash_node_s *next;
 } hash_node_t;
 
 /**
@@ -25,13 +25,13 @@ typedef struct hash_node_s
  *
  * @size: The size of the array
  * @array: An array of size @size
- * Each cell of this array is a pointer to the first node of a linked list,
- * because we want our HashTable to use a Chaining collision handling
+ *         Each cell of this array is a pointer to the first node of a linked list,
+ *         because we want our HashTable to use a Chaining collision handling
  */
 typedef struct hash_table_s
 {
-     unsigned long int size;
-     hash_node_t **array;
+	unsigned long int size;
+	hash_node_t **array;
 } hash_table_t;
 
 hash_table_t *hash_table_create(unsigned long int size);
@@ -42,4 +42,4 @@ char *hash_table_get(const hash_table_t *ht, const char *key);
 void hash_table_print(const hash_table_t *ht);
 void hash_table_delete(hash_table_t *ht);
 
-#endif /*HASH_TABLES_H*/
+#endif /* HASH_TABLES_H */
